224d99918805edf2f22ae5b69b27a601a9f3013c
froissak <kevin.froissart69@gmail.com>
2019-09-16 14:13:02 +0200

    ihm main

---
+++ src/Project/Main.java
@@ -0,0 +1,27 @@
+package Project;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public void start(Stage stage) throws Exception {
+		
+		VBox root = new VBox();
+		
+		Scene scene = new Scene(root,480,580);
+
+		stage.setResizable(false);
+		stage.setTitle("Solar System Simulator");
+		stage.setScene(scene);
+		
+		stage.centerOnScreen();
+		stage.show();
+	}
+	
+	public static void main(String[] a){
+		Application.launch(a);
+	}
+}

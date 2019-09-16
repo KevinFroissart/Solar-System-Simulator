4f90b151c12912b34e7ce14166e270fed4e45c5c
derouck <derouckl@ayou07.iut-infobio.priv.univ-lille1.fr>
2019-09-16 14:33:21 +0200

    planete

---
+++ src/model/Planete.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Planete {
+	private String name;
+	private int masse;
+	private Position pos;
+	
+	
+	public Planete(String name,int masse,Position pos) {
+		this.masse=masse;
+		this.name=name;
+		this.pos=pos;
+		
+	}
+}

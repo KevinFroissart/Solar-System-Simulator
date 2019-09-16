3873dcd9b6faea7cbd32419493bf5304c0f4d8b9
derouck <derouckl@ayou07.iut-infobio.priv.univ-lille1.fr>
2019-09-16 14:36:22 +0200

    planete get and set

--- src/model/Planete.java
+++ src/model/Planete.java
@@ -12,4 +12,35 @@ public class Planete {
 		this.pos=pos;
 		
 	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getMasse() {
+		return masse;
+	}
+
+
+	public void setMasse(int masse) {
+		this.masse = masse;
+	}
+
+
+	public Position getPos() {
+		return pos;
+	}
+
+
+	public void setPos(Position pos) {
+		this.pos = pos;
+	}
+	
 }

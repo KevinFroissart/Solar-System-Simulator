9e3afaf1b5c0e4ef4bc4816f995ea46661017d1b
Froissart Kévin <Froissart Kvin@169.254.141.247>
2019-09-17 20:46:48 +0200

    System Loader peut lire le fichier, analyser les données et stocker les paramètres

--- src/Controleur/SystemLoader.java
+++ src/Controleur/SystemLoader.java
@@ -5,21 +5,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SystemLoader {
 
-	static String system = "";
+	static ArrayList<String> lignes;
 	static double G;
 	static double dt;
 	static double fa;
 	static double rayon;
 
 	public static void reader() {
+
+		lignes = new ArrayList<String>();
+
 		try {
 			File file = new File("ressources/system.txt");
 			BufferedReader br = new BufferedReader(new FileReader(file));
 			String read = "";
-			while ((read = br.readLine()) != null) system += read + ";";
+			while ((read = br.readLine()) != null) {
+				read += ";";
+				lignes.add(read);
+			}
 			br.close();
 		}
 		catch(FileNotFoundException e) {
@@ -28,38 +35,54 @@ public class SystemLoader {
 		catch(IOException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	public static void paramInit() {
-		String param = system;
-
-		int cpt = 0;
-
-		while(param.charAt(cpt) != ';') {
-			if(system.charAt(cpt) == 'G') {
-				G = 0.0001;
-			}
-			if(system.substring(cpt,cpt+2).equals("fa")) {
-				System.out.println("oui2");
-
-			}
-			if(system.substring(cpt,cpt+2) == "dt") {
-				System.out.println("oui3");
-
-			}
-			if(system.substring(cpt,cpt+5) == "rayon") {
-				System.out.println("oui4");
-
+		
+		for(int i = 0; i < lignes.size(); i++) {
+		
+			int cpt = 0;
+			int lim = lignes.get(i).length();
+			
+			while(cpt < lim) {
+				
+				if(cpt+1 < lim && lignes.get(i).substring(cpt,cpt+1).equals("G")) {
+					G = Double.parseDouble(wordReader(cpt, lim, lignes.get(i),'=',' '));
+				}
+				if(cpt+2 < lim && lignes.get(i).substring(cpt,cpt+2).equals("dt")) {
+					dt = Double.parseDouble(wordReader(cpt, lim, lignes.get(i),'=',' '));
+				}
+				if(cpt+2 < lim && lignes.get(i).substring(cpt,cpt+2).equals("fa")) {
+					fa = Double.parseDouble(wordReader(cpt, lim, lignes.get(i),'=',' '));
+				}
+				if(cpt+5 < lim && lignes.get(i).substring(cpt,cpt+5).equals("rayon")) {
+					rayon = Double.parseDouble(wordReader(cpt, lim, lignes.get(i),'=',' '));
+				}
+				cpt++;
 			}
 		}
-
 	}
 
+	public static String wordReader(int idx, int lim, String txt, char beg, char end) {
+		int debut = 0;
+		int fin = 0;
+		for(int i = idx; i < lim; i++) {
+			if(txt.charAt(i) == beg) debut = i+1;
+			if(txt.charAt(i) == end || txt.charAt(i) == ';') {
+				fin = i;
+				break;
+			}
+		}
+		return txt.substring(debut,fin);
+	}
+	
 	public static void main(String[] args) {
 		reader();
-		System.out.println(system);
 		paramInit();
 		System.out.println(G);
+		System.out.println(dt);
+		System.out.println(fa);
+		System.out.println(rayon);
 	}
 }

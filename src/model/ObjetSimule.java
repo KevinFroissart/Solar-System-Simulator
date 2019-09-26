06c5bcec04c21346d892cfdb45c9906b20d49134
Derouck Lucas <derouckl@epicea05.iut-infobio.priv.univ-lille1.fr>
2019-09-26 14:21:25 +0200

    ajout objet simule

--- src/model/ObjetSimule.java
+++ src/model/ObjetSimule.java
@@ -1,5 +1,8 @@
 package model;
 
+/** Classe définissant un objet dans le système, comme une planète.
+ * @author //compléter Lucas
+ */
 public class ObjetSimule extends Objet {
 
 	public ObjetSimule(String name, double masse, Vecteur pos) {
--- src/model/Vaisseau.java
+++ src/model/Vaisseau.java
@@ -1,5 +1,8 @@
 package model;
 
+/** Classe définissant un vaisseau dans le système.
+ * @author //compléter Lucas
+ */
 public class Vaisseau extends Objet{
 	private static double masse=0.001;
 	public Vaisseau(String name, Vecteur pos) {

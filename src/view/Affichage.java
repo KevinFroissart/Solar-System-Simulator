bc7e10adb049674c0cdc402a3abd901d0b23dc0d
Bisson Clément <clement.bisson.etu@univ-lille.fr>
2019-09-26 14:08:05 +0200

    Modification Affichage.java

--- src/view/Affichage.java
+++ src/view/Affichage.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 	/** Classe gérant l'affichage principal du système.
@@ -25,6 +26,14 @@ public class Affichage{
 		stage.setTitle("Solar System Simulator");
 		stage.setScene(scene); 
 		root.getChildren().add(canvas);
+		gc.setFill(Color.LIGHTSEAGREEN);
+		gc.fillOval(240, 300, 15, 15);
+		gc.setFill(Color.DARKRED);
+		gc.fillOval(240, 250, 5, 5);
+		gc.fillOval(190, 330, 5, 5);
+		gc.fillOval(350, 300, 5, 5);
+		
+		
 		
 		stage.centerOnScreen();
 		stage.show();

1d2a79839572004e117af571080bfccf6e373904
kururmi <derouckl@epicea05.iut-infobio.priv.univ-lille1.fr>
2019-09-26 14:02:24 +0200

    creation de la classe vaisseau et changement des int de la classe vecteur

---
+++ src/model/Vaisseau.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Vaisseau extends Objet{
+
+	public Vaisseau(String name, double masse, Vecteur pos) {
+		super(name, masse, pos);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}

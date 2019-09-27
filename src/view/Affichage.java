c69f1df38159e1a29ab1e8cf4d34a27a15dafe1b
Clément Bisson <Clment@DESKTOP-4CVUEHU>
2019-09-27 23:29:23 +0200

    Transformation canvas en group, création de fonction créer vaisseau/planete/etoile, début doc

--- src/view/Affichage.java
+++ src/view/Affichage.java
@@ -1,69 +1,69 @@
 package view;
 
-import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 
 /** Classe g√©rant l'affichage principal du syst√®me.
  * @author Froissart Kevin
  */
 
 public class Affichage{
+	/**
+	 * Constructeur de la vue du vaisseau. 
+	 * Cette fonction instancie le vaisseau aux coordonnÈes indiquÈ en paramËtre
+	 * @param x indique la position x initiale du vaisseau ‡ son instanciation
+	 * @param y indique la position y initiale du vaisseau ‡ son instanciation 
+	 * @param root layer racine de l'IHM
+	 */
+	public void createSpaceShip(int x, int y , Group root){
 
-	public void createSpaceShip(GraphicsContext gc /*aujout√© coordonn√©es*/){
-		/*
-		Polygon triangle = new Polygon();  
-		triangle.getPoints().addAll(new Double[]{  
-				0.0, 0.0,  
-				100.0, 200.0,  
-				200.0, 100.0 });  
-	*/
-		//gc.fillPolygon(new Double[]{50.0,100.0,150.0}, new Double[]{50.0,100.0,150.0}, 3);
-		
-		Polygon triangle = new Polygon();
-		triangle.getPoints().setAll(
-		   50.0, 50.0,
-		   60.0, 60.0,
-		   20.0, 40.0
-		);
-
+		Rectangle vaisseau = new Rectangle(x, y,7,3);
+		vaisseau.setFill(Color.RED);
+		root.getChildren().add(vaisseau);
 	}
 
+	public void createSun(int x, int y ,Group root){
+
+		Circle sun = new Circle(x,y,15);
+		//sun.setFill(null);
+		root.getChildren().addAll(sun);
+	}
+
+	public void createPlanete(int x, int y, Group root ){
+
+		Circle planete = new Circle(x,y,5);
+		//planete.setFill(null);
+		root.getChildren().addAll(planete);
+	}
 
 	public void start(Stage stage) throws Exception {
+		Group root = new Group();
+		Scene scene = new Scene(root, 480, 580);
 
-		VBox root = new VBox();
 
-		Scene scene = new Scene(root,480,580);
-		final Canvas canvas = new Canvas(480,580);
-		GraphicsContext gc = canvas.getGraphicsContext2D();
+
 
 		stage.setResizable(false);
 		stage.setTitle("Solar System Simulator");
 		stage.setScene(scene); 
-		root.getChildren().add(canvas);
-		gc.setFill(Color.LIGHTSEAGREEN);
-		gc.fillOval(240, 300, 15, 15);
-		gc.setFill(Color.DARKRED);
-		gc.fillOval(240, 250, 5, 5);
-		gc.fillOval(190, 330, 5, 5);
-		gc.fillOval(350, 300, 5, 5);
 
+
+		// forme exemple :
+		Circle p1 = new Circle(240,300,15);
+		p1.setFill(Color.LIGHTSEAGREEN);
+		Circle p2 = new Circle(240,250,5);
+		Circle p3 = new Circle(190,330,5);
+		Circle p4 = new Circle(350,300,5);
 		
-		Polygon triangle = new Polygon();
-		triangle.getPoints().addAll(
-		   150.0, 150.0,
-		   160.0, 160.0,
-		   120.0, 140.0
-		);
-		
-		
-		root.getChildren().add(triangle);
+		createSun(300, 200, root);
+		createPlanete(100, 400, root);
+		createSpaceShip(80, 80, root);
+
+		root.getChildren().addAll(p1,p2,p3,p4);
 
 		stage.centerOnScreen();
 		stage.show();

652b3662473e0c95bec086360956bdac5288dcf9
Maxence Bourdin <maxence.bourdin2.etu@univ-lille.fr>
2019-09-30 14:37:56 +0200

    ajout des méthodes de direction dans le control vaisseau

--- src/controller/VaisseauControl.java
+++ src/controller/VaisseauControl.java
@@ -1,8 +1,51 @@
 package controller;
 
+import model.Objet;
+import model.SystemLoader;
+import model.Systeme;
+import model.Vecteur;
+
 /** Cette classe contient les méthodes permettant le contrôle de la fusée dans l'interface.
  * @author Maxence
  */
 public class VaisseauControl {
     //TODO : construire le controleur de la saisie de contrôle du vaisseau
+	
+	SystemLoader sl;
+	Systeme sys;
+	
+	VaisseauControl(SystemLoader s, Systeme sys) {
+		sl = s;
+		this.sys = sys;
+	}
+	
+	public void up(Objet obj, double value) {
+		obj.setPos(new Vecteur(0, -value));
+	}
+	
+	public void down(Objet obj, double value) {
+		obj.setPos(new Vecteur(0, value));
+	}
+	
+	public void left(Objet obj, double value) {
+		obj.setPos(new Vecteur(-value, 0));
+	}
+	
+	public void right(Objet obj, double value) {
+		obj.setPos(new Vecteur(value, 0));
+	}
+
+	public SystemLoader getModel() {
+		return sl;
+	}
+
+	public Systeme getSysteme() {
+		return sys;
+	}
+	
+	
+	
+	
+	
+	
 }

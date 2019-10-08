c6024712906c0714cb07c68d78f898c3c3030b39
Froissart Kévin <kevin.froissart69@gmail.com>
2019-10-08 13:48:59 +0200

    modification des vues, liste dynamique, fonctionne sur les deux vues

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 import controller.AffichageControl;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -15,22 +17,23 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
+		ArrayList<Objet> listeObjet;
+		
 		SystemLoader sl = new SystemLoader();
-		sl.reader();	
+		sl.reader();
+		listeObjet = sl.objectInit();
 		Systeme sys = sl.paramInit(4);
-
 	
-		Affichage af = new Affichage(new AffichageControl(sl, sys));
-		Information info = new Information(sl,sys);
+		Information info = new Information(listeObjet);
+		Affichage af = new Affichage(new AffichageControl(sl, sys),info,listeObjet);
 
-		for(Objet o : sl.objectInit()) {
+		for(Objet o : listeObjet) {
 			o.addObserver(af);
 			o.addObserver(info);
 		}
 		
 		try {
 			af.start(primaryStage);
-			info.start();
 		} catch (Exception e1) {
 			e1.printStackTrace();
 

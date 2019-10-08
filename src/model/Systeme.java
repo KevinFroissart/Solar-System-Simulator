c9725cfc8e8c7f36c2ef131e4cab22b6828f6c23
Bisson Clément <clement.bisson.etu@univ-lille.fr>
2019-10-08 14:44:07 +0200

    javadoc

--- src/model/Systeme.java
+++ src/model/Systeme.java
@@ -16,7 +16,14 @@ public class Systeme {
 	private double dt = 0;
 	private double fa = 0;
 	private double rayon = 0;
-
+	
+	/**
+	 * Contructeur qui instancie le Systeme à l'aide des valeurs de chacun des attributs mis en paramètre
+	 * @param g
+	 * @param dt
+	 * @param fa
+	 * @param rayon
+	 */
 	public Systeme(double g, double dt, double fa, double rayon) {
 		this.g = g;
 		this.dt = dt;
@@ -24,21 +31,34 @@ public class Systeme {
 		this.rayon = rayon;
 	}
 
+	/**
+	 * Retourne la valeur de la constante de gravité
+	 * @return g
+	 */
 	public double getG() {
 		return g;
 	}
 
-
+	/**
+	 * Retourne la valeur du pas de temps du systeme
+	 * @return valeur de dt du Systeme
+	 */
 	public double getDt() {
 		return dt;
 	}
 
-
+	/**
+	 * Retourne la valeur du facteur d'accélération du systeme
+	 * @return valeur du (fa) facteur d'accélération du systeme 
+	 */
 	public double getFa() {
 		return fa;
 	}
 
-
+	/**
+	 * Retourne la valeur du rayon du Systeme
+	 * @return valeur du rayon du systeme
+	 */
 	public double getRayon() {
 		return rayon;
 	}	

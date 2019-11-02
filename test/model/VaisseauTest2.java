3626e6d4a19e79b5b6f61ee72f6a61e539c54a8c
Froissart Kévin <kevin.froissart69@gmail.com>
2019-11-02 16:02:26 +0100

    Préparation du rendu

---
+++ test/model/VaisseauTest2.java
@@ -0,0 +1,59 @@
+package model;
+/*import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import model.Vaisseau;
+import model.Vecteur;
+
+public class VaisseauTest {
+	Vaisseau Starship = new Vaisseau("Vaisseau", "Soyouz", 3.0, new Vecteur(0.0,0.0), new Vecteur(1.0,1.0),new Vecteur(2.0,2.0),20.0);
+	
+
+	if (Starship.getName()== "Vaisseau" && Starship.getType()== "Soyouz" && Starship.getPos().equals(new Vecteur(0.0,0.0)) && Starship.getMasse()== 3.0 &&
+		Starship.getVitesse().equals(new Vecteur(1.0,1.0)))
+	{
+
+		assertTrue(false);
+	}
+
+	
+	@Test
+	public void VaisseauUpTest() {
+		Starship.up(20.0);
+		double res = Starship.getVitesse().getPosY() ;
+		if (res != 21.0) {
+			assertTrue(false);
+		}
+	}
+	
+	
+	@Test
+	public void VaisseauDownTest() {
+		Starship.down(20.0);
+		double res = Starship.getVitesse().getPosY() ;
+		if (res != -19.0) {
+			assertTrue(false);
+		}
+	}
+	
+
+	@Test
+	public void VaisseauLeftTest() {
+		Starship.left(20.0);
+		double res = Starship.getVitesse().getPosX() ;
+		if (res != 21.0) {
+			assertTrue(false);
+		}
+	}
+	
+	@Test
+	public void VaisseauRightTest() {
+		Starship.right(20.0);
+		double res = Starship.getVitesse().getPosX() ;
+		if (res != -19.0) {
+			assertTrue(false);
+		}
+	}
+}
+*/

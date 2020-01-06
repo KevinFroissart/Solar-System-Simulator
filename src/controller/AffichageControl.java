f37160cd04f0cab061a14bbdc1d8acc610803adc
froissak <kevin.froissart.etu@univ-lille.fr>
2020-01-06 11:39:36 +0100

    ajout du chrono, implémenté sur le calcul des positions et le cercle

--- src/controller/AffichageControl.java
+++ src/controller/AffichageControl.java
@@ -76,12 +76,12 @@ public class AffichageControl extends Observable {
 	/**
 	 * Changer la position de l'objet en paramètre
 	 */
-	public void pos(Objet obj) { //TO:DO ajouter le temps t à la signature et au corps de la fonction
+	public void pos(Objet obj, int t) { //TO:DO ajouter le temps t à la signature et au corps de la fonction
 		if(!obj.getType().equals("Cercle") && !obj.getType().equals("Ellipse")) {
 			obj.setPos(new Vecteur(obj.getPos().getPosX() + obj.getVitesse().getPosX(), obj.getPos().getPosY() + obj.getVitesse().getPosY()));
 		} else if(obj.getType().equals("Cerlce")) {
 			ObjetCercle o = (ObjetCercle) obj;
-			double angle = 2*Math.PI / (/*t%*/ o .getPeriode());
+			double angle = 2*Math.PI / (t%o.getPeriode());
 			double x2 = Math.pow(o.getPos().getPosX()-o.getCentre().getPos().getPosX(),2);
 			double y2 = Math.pow(o.getPos().getPosY()-o.getCentre().getPos().getPosY(),2);
 			double x = o.getCentre().getPos().getPosX() + Math.sqrt(x2+y2) * Math.cos(angle);

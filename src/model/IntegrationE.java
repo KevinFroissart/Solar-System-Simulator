96bb4d0f677a95a15585003df85a7387c35c7df4
bourdinm <maxence.bourdin2.etu@univ-lille.fr>
2020-01-12 16:31:44 +0100

    Affichage de la méthode en cours d'utilisation, implémentation de leapfrog, bouton pour changer la méthode kevin

--- src/model/IntegrationE.java
+++ src/model/IntegrationE.java
@@ -32,17 +32,24 @@ public interface IntegrationE {
         double yB = objB.getPos().getPosY();
         double distance = Math.sqrt(Math.pow(xB - xA, 2) + Math.pow(yB - yA, 2));
         double force = sys.getG()* (objA.getMasse() * objB.getMasse()) / Math.pow(distance, 2);
-        Vecteur AB = new Vecteur(objA.getPos(), objB.getPos());
-        double norme = AB.getNorme();
-        Vecteur acc = new Vecteur(AB.getPosX() / norme * force, AB.getPosY() / norme * force);
+        double acc = force / objA.getMasse();
+
+        if(objA.getType().equals("Vaisseau")) {
+            if(objB.getType().equals("Fixe")) objA.setAttractionSoleil((sys.getG() * objA.getMasse() * objB.getMasse()) / Math.pow(distance, 2));
+            if(objB.getType().equals("Simulé")) objA.setAttractionPlanete((sys.getG() * objA.getMasse() * objB.getMasse()) / Math.pow(distance, 2));
+            objA.setAttraction((objA.getAttractionPlanete() + objA.getAttractionSoleil()) / 2);
+        }
 
         Vecteur demiVit = new Vecteur(
-                objA.getVitesse().getPosX() + (sys.getDt() * acc.getPosX() / 2),
-                objA.getVitesse().getPosY() + (sys.getDt() * acc.getPosY() / 2)
+                objA.getVitesse().getPosX() + (sys.getDt() * acc / 2),
+                objA.getVitesse().getPosY() + (sys.getDt() * acc / 2)
         );
 
-        objA.setPos(new Vecteur(objA.getPos().getPosX() + sys.getDt() * demiVit.getPosX(), objA.getPos().getPosY() + sys.getDt() * demiVit.getPosY()));
-        objA.setVit(new Vecteur(objA.getPos().getPosX() + sys.getDt() * acc.getPosX(), objA.getPos().getPosY() + sys.getDt() * acc.getPosY()));
+        double dirX = (xB - xA) / distance;
+        double dirY = (yB - yA) / distance;
+        double vitX = demiVit.getPosX() + dirX * acc;
+        double vitY = demiVit.getPosY() + dirY * acc;
+        objA.setVit(new Vecteur(vitX, vitY));
     }
 
     public static void RK4(Objet objA, Objet objB, Systeme sys){
